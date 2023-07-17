fb20fc2bb85014c092af479142dabaaf95159567
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-07-17 15:49:12 +0300

    add get_vacancy_by_salary_to method

--- src/add_vacancies_class.py
+++ src/add_vacancies_class.py
@@ -51,7 +51,13 @@ class JsonSaverVacancy(JsonSaver):
                 vacancy_list_by_salary.append(item)
         return vacancy_list_by_salary
     def get_vacancy_by_salary_to(self, search, vacancy):
-        pass
+        vacancy_list_by_salary = []
+        for item in vacancy:
+            if type(item["ЗП"]["До"]) == str:
+                continue
+            elif item["ЗП"]["До"] == int(search):
+                vacancy_list_by_salary.append(item)
+        return vacancy_list_by_salary
 
     def get_vacancy_by_experience(self, search):
         pass
@@ -62,31 +68,35 @@ class JsonSaverVacancy(JsonSaver):
     def delete_vacancy(self, vacancy):
         pass
 
-def hh_exmpl():
-    hh = HeadHunter()
-    get_vacancy = hh.get_vacancy("Python")
-    return get_vacancy
-
-def norm_vid():
-    new_list = []
-    for item in hh_exmpl():
-        vacancy = Vacancy(item)
-        new_list.append(vacancy.create_dict())
-    return new_list
-
-def json_asd():
-    a = JsonSaverVacancy()
-    a.add_vacancy_to_file(norm_vid())
-    return a
-def json_exmpl():
-    a = JsonSaverVacancy()
-    return a.get_vacancy_by_salary_average(20000, norm_vid())
-
-def salary_from():
-    a = JsonSaverVacancy()
-    return a.get_vacancy_by_salary_from(20000, norm_vid())
-
-print(salary_from())
+# def hh_exmpl():
+#     hh = HeadHunter()
+#     get_vacancy = hh.get_vacancy("Учитель")
+#     return get_vacancy
+#
+# def norm_vid():
+#     new_list = []
+#     for item in hh_exmpl():
+#         vacancy = Vacancy(item)
+#         new_list.append(vacancy.create_dict())
+#     return new_list
+#
+# def json_asd():
+#     a = JsonSaverVacancy()
+#     a.add_vacancy_to_file(norm_vid())
+#     return a
+# def json_exmpl():
+#     a = JsonSaverVacancy()
+#     return a.get_vacancy_by_salary_average(20000, norm_vid())
+#
+# def salary_from():
+#     a = JsonSaverVacancy()
+#     return a.get_vacancy_by_salary_from(20000, norm_vid())
+#
+# def salary_to():
+#     a = JsonSaverVacancy()
+#     return a.get_vacancy_by_salary_to(60000, norm_vid())
+#
+# print(salary_to())
 # print(json_exmpl())
 # print(get_vacancy)
 # new_list = []
